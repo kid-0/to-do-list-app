ecabe6a3b00a35ae969bfbb4723c95106eb7a36c
kid-0 <64641404+kid-0@users.noreply.github.com>
2023-01-31 15:35:40 +0100

    rewrote the code to cross out items in jQuery

--- js/script.js
+++ js/script.js
@@ -14,5 +14,11 @@ function newItem(){
         $('#list').append(li);
     }
 
+    //2. Crossing out an item from the list of items:
+    
+    li.on('dblclick', function crossOut() {
+        li.toggleClass('strike');
+    });
+
 
 }
